b5efee632531961029b9f20379983f774719c849
alekseyburn <alekseyburn@gmail.com>
2019-06-11 22:41:53 +0300

    сделал анимацию появления блоков, lazyload

--- source/js/loadScripts.js
+++ source/js/loadScripts.js
@@ -8,3 +8,22 @@ let loadJQuery = function () {
 let loadSlick = function () {
   loadFile("slick.min.js");
 };
+
+let loadCss = function (file) {
+  let fileRef = document.createElement("link");
+
+  fileRef.setAttribute("rel", "stylesheet");
+  fileRef.setAttribute("href", file);
+  document.getElementsByTagName("head")[0].appendChild(fileRef);
+};
+
+let loadSlickCss = function() {
+  loadCss("css/slick.min.css");
+};
+
+let loadRevealatorCss =function() {
+  loadCss("css/fm.revealator.jquery.min.css");
+};
+
+loadSlickCss();
+loadRevealatorCss();
